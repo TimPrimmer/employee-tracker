50886cbdc330203b3ae07c081c416e93de932dc1
Tim Primmer <timprimmer@gmail.com>
2021-07-31 15:16:03 -0500

    improved inquirer loop display, employees can now be added

--- index.js
+++ index.js
@@ -1,34 +1,85 @@
 const db = require('./db/connection');
 const inquirer = require('inquirer');
-const questions = require("./utils/questions.js");
+const prompts = require("./utils/questions.js");
 const cTable = require('console.table');
 
 
 
 const displayData = (sqlrows) => {
-  console.clear();
+  console.log("\n");
   console.table(sqlrows);
-  for (let x = 0; x < sqlrows.length; x++) {
-    console.log("\n");
-  }
+
 }
 
 // main menu inquirer loop which returns another call to this function with a new inquirer, only exits if the user select option 3
 const mainMenu = async () => {
-  const answers = await inquirer.prompt(questions.menu);
+  const answers = await inquirer.prompt(prompts.questions.menu);
 
-  if (answers.selection.charAt(0) === "1") {
+  if (answers.selection.charAt(0) === "1") { // View all departments
     const sql = `SELECT * FROM department`;
     db.query(sql, (err, rows) => {
       displayData(rows);
+      return mainMenu();
     });
   }
 
-  else if (answers.selection.charAt(0) === "8") {
+  else if (answers.selection.charAt(0) === "2") { // View all roles
+    const sql = `SELECT * FROM role`;
+    db.query(sql, (err, rows) => {
+      displayData(rows);
+      return mainMenu();
+    });
+  }
+
+  else if (answers.selection.charAt(0) === "3") { // View all employees
+    const sql = `SELECT * FROM employee`;
+    db.query(sql, (err, rows) => {
+      displayData(rows);
+      return mainMenu();
+    });
+  }
+
+  else if (answers.selection.charAt(0) === "4") { // Add a department
+    const sql = `SELECT * FROM department`;
+    db.query(sql, (err, rows) => {
+      displayData(rows);
+      return mainMenu();
+    });
+  }
+
+  else if (answers.selection.charAt(0) === "5") { // Add a role
+    const sql = `SELECT * FROM department`;
+    db.query(sql, (err, rows) => {
+      displayData(rows);
+      return mainMenu();
+    });
+  }
+
+  else if (answers.selection.charAt(0) === "6") { // Add an employee
+    const answers = await inquirer.prompt(prompts.questions.addEmployee);
+    const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES (?,?,?,?)`;
+    const params = [answers.fname, answers.lname, Number(answers.role.split('.')[0]), Number(answers.manager.split('.')[0])];
+
+    db.query(sql, params, (err, result) => {
+      console.log(`Employee ${answers.fname} ${answers.lname} has now been added!`)
+      prompts.gatherInfo(); // updates the questions to reflect our new info
+      return mainMenu();
+    });
+  }
+
+  else if (answers.selection.charAt(0) === "7") { // Update an employee role
+    const sql = `SELECT * FROM department`;
+    db.query(sql, (err, rows) => {
+      displayData(rows);
+      return mainMenu();
+    });
+  }
+
+  else if (answers.selection.charAt(0) === "8") { // Quit
     process.exit(0); // closes the node program
   }
 
-  return mainMenu();
 };
 
 mainMenu();
--- utils/questions.js
+++ utils/questions.js
@@ -1,3 +1,30 @@
+const db = require('../db/connection');
+const employeeList = [];
+const roleList = [];
+
+const getEmployees = () => {
+  const sql = `SELECT * FROM employee`;
+  db.query(sql, (err, rows) => {
+    rows.forEach((employee) => { 
+      employeeList.push(`${employee.id}. ${employee.first_name} ${employee.last_name}`);
+    })
+  });
+};
+
+const getRoles = () => {
+  const sql = `SELECT * FROM role`;
+  db.query(sql, (err, rows) => {
+    rows.forEach((role) => { 
+      roleList.push(`${role.id}. ${role.title}`);
+    })
+  });
+};
+
+const gatherInfo = () => {
+  getEmployees();
+  getRoles();
+}
+
 // questions for the main menu
 const mainMenuQuestions = [
   {
@@ -5,8 +32,8 @@ const mainMenuQuestions = [
     name: "selection",
     message: "What would you like to do?",
     choices: [
-      "1. View all departments", 
-      "2. View all roles", 
+      "1. View all departments",
+      "2. View all roles",
       "3. View all employees",
       "4. Add a department",
       "5. Add a role",
@@ -17,9 +44,53 @@ const mainMenuQuestions = [
   }
 ];
 
+// questions for adding an employee
+const addEmployeeQuestions = [
+  {
+    type: "input",
+    name: "fname",
+    message: "What is the employees first name?",
+    validate(value) {
+      if (!value) {
+        console.log("please enter in a first name!")
+        return false;
+      }
+      else { return true; }
+    }
+  },
+  {
+    type: "input",
+    name: "lname",
+    message: "What is the employees last name?",
+    validate(value) {
+      if (!value) {
+        console.log("please enter in a last name!")
+        return false;
+      }
+      else { return true; }
+    }
+  },
+  {
+    type: "list",
+    name: "role",
+    message: "What is the employees role?",
+    choices: roleList
+  },
+  {
+    type: "list",
+    name: "manager",
+    message: "Who is the employees manager?",
+    choices: employeeList
+  }
+];
+
+// run the gatherInfo function so we can get our current employees and roles for use in the questions
+gatherInfo();
+
 // questions object holding all question sets
 const questions = {
-  menu: mainMenuQuestions
+  menu: mainMenuQuestions,
+  addEmployee: addEmployeeQuestions
 }
 
-module.exports = questions;
+module.exports = {questions, gatherInfo};
